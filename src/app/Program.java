427029f0b99e2c010b983e092bb8d767932e92db
Isaque_Soares <isaquesbl123@gmail.com>
2024-12-21 23:29:11 -0300

    Created " Program Classe "

--- src/app/Program.java
+++ src/app/Program.java
@@ -2,5 +2,8 @@ package app;
 
 public class Program {
     
-    System.out.println("Hello world!");
+    public static void main(String[] args) {
+        
+        System.out.println("Hello Word!");
+    }
 }
